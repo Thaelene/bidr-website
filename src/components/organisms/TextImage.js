c290330b7d31b1d964eac8667c4b788f95e4176e
thaelene <helene.ta@hetic.net>
2020-05-25 18:28:52 +0200

    :sparkles: [TextImage] Add textImage component

--- src/components/organisms/TextImage.js
+++ src/components/organisms/TextImage.js
@@ -1,17 +1,38 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
 
+import Image from "../atoms/Image"
 import Layout from "../atoms/Layout"
-import Header from "../molecules/Header"
 
 import styles from "./textImage.module.css"
 
-const TextImage = ({ imgPosition }) => {
+const TextImage = ({ imgPosition, img, imgAlt, subtitle, title, text }) => {
   return (
-    <section className={styles[`${imgPosition}Image`]}>
-      <p>Super texte gauche</p>
-      <p>Super texte droite</p>
+    <section
+      className={[styles[`${imgPosition}Image`], styles.textImageSection].join(
+        " "
+      )}
+    >
+      <Layout>
+        <div
+          className={[
+            styles[`${imgPosition}Image`],
+            styles.textImageContainer,
+          ].join(" ")}
+        >
+          <div className={styles.textImageContentWrapper}>
+            <h4>{subtitle}</h4>
+            <h2>{title}</h2>
+            <p>{text}</p>
+          </div>
+          <div className={styles.textImageWrapper}>
+            <Image
+              imgsrc={img}
+              imgAlt={imgAlt}
+              stylesImage={styles.stylesImage}
+            />
+          </div>
+        </div>
+      </Layout>
     </section>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,22 @@ const App = () => {
   return (
     <div>
       <Hero />
-      <TextImage imgPosition="right" />
+      <TextImage
+        imgPosition="left"
+        img="auctionnerHammer.png"
+        imgAlt="Auctionner's hammer in Bidr"
+        subtitle="Les dernières tendances"
+        title="De nouvelles enchères toutes les semaines"
+        text="Avec Bidr, découvrez chaque semaine les marques du moment et prenez part à l’enchère pour espérer repartir avec des produits exclusifs pour presque rien.  "
+      />
+      <TextImage
+        imgPosition="right"
+        img="host.png"
+        imgAlt="Bidr's host"
+        subtitle="L'animation"
+        title="Notre animateur vedette veillera au grain"
+        text="Chaque événement a lieu en direct, l’animateur sera en charge du bon déroulement de l’enchère et se fera un plaisir de vous informer sur les produits."
+      />
       <p> Welcome to Bidr!</p>
     </div>
   )
